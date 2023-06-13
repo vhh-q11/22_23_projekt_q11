ae26a2a6b756996a529cf10dfc8c088083030161
Jochen Schmitt <tst@gymvhh.de>
2023-06-14 00:20:53 +0200

    3

--- mqtt.js
+++ mqtt.js
@@ -1,35 +1,3 @@
-class Mqtt {
-  constructor() {
-    this.clientid =
-      "vhh" + new Date().getMilliseconds() + new Date().getSeconds();
 
-    this.client = new Paho.MQTT.Client(
-      "mqtt.eclipseprojects.io",
-      443,
-      clientid
-    );
-
-    this.client.onConnectionLost = this.onConnectionLost;
-    this.client.onMessageArrived = this.onMessageArrived;
-    this.client.connect({ onSuccess: this.onConnect });
-  }
-
-  onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
-    console.log("onConnect");
-    this.client.subscribe("/World");
-    message = new Paho.MQTT.Message("Hello");
-    message.destinationName = "/World";
-    this.client.send(message);
-  }
-  onConnectionLost(responseObject) {
-    if (responseObject.errorCode !== 0)
-      console.log("onConnectionLost:" + responseObject.errorMessage);
-  }
-  onMessageArrived(message) {
-    console.log("onMessageArrived:" + message.payloadString);
-    this.client.disconnect();
-  }
-}
 
 console.log("mqtt class loaded");
