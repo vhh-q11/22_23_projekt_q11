56eb916fc82d1a9a1448dae938649a649f066318
vhh-q11 <133121252+vhh-q11@users.noreply.github.com>
2023-06-15 12:24:53 +0200

    Update mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -1,66 +1,64 @@
 
 
-  function connect() {
-    clientid =
-      "vhh" + new Date().getMilliseconds() + new Date().getSeconds();
+function connect() {
+  clientid =
+    "vhh" + new Date().getMilliseconds() + new Date().getSeconds();
 
-    client = new Paho.MQTT.Client(
-      "mqtt.eclipseprojects.io",
-      443,
-      clientid
-    );
+  client = new Paho.MQTT.Client(
+    "mqtt.eclipseprojects.io",
+    443,
+    clientid
+  );
 
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageArrived;
-    var options = {
-      timeout: 3,
-      useSSL: true,
-      cleanSession: true,
-      reconnect: true,
-      onSuccess: onConnect,
-      onFailure: function (message) {
-        console.log(
-          new Date().toUTCString() +
-            "Connection failed: " +
-            message.errorMessage +
-            "Retrying"
-        );
-      },
-    };
-    // connect the client
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  var options = {
+    timeout: 3,
+    useSSL: true,
+    cleanSession: true,
+    reconnect: true,
+    onSuccess: onConnect,
+    onFailure: function (message) {
+      console.log(
+        new Date().toUTCString() +
+        "Connection failed: " +
+        message.errorMessage +
+        "Retrying"
+      );
+    },
+  };
+  // connect the client
 
-    client.connect(options);
-  }
+  client.connect(options);
+}
 
-  function onConnect() {
-    isConnected = "ja";
-    console.log("function onConnect");
-    listener.test();
-  }
+function onConnect() {
+  isConnected = "ja";
+  console.log("function onConnect");
+  listener.test();
+}
 
-  function onConnectionLost(responseObject) {
-    console.log("connection lost");
+function onConnectionLost(responseObject) {
+  console.log("connection lost");
 
-    if (responseObject.errorCode !== 0)
-      console.log("onConnectionLost:" + responseObject.errorMessage);
-  }
+  if (responseObject.errorCode !== 0)
+    console.log("onConnectionLost:" + responseObject.errorMessage);
+}
 
-  function onMessageArrived(message) {
-    console.log(isConnected);
-    console.log("onMessageArrived:" + message.payloadString);
-  }
+function onMessageArrived(message) {
+   console.log("onMessageArrived:" + message.payloadString);
+}
 
-  function sendMessage(text, topic) {
-    console.log("sendMessage");
-    console.log(isConnected);
-    var message = new Paho.MQTT.Message(text);
-    message.destinationName = topic;
-    client.send(message);
-  }
 
-  function subscribe(topic) {
-    isConnected += ".";
-    console.log(isConnected);
-    client.subscribe(topic);
-  }
+
+
+function sendMessage(text, topic) {
+  var message = new Paho.MQTT.Message(text);
+  message.destinationName = topic;
+  client.send(message);
+}
+
+function subscribe(topic) {
+  client.subscribe(topic);
+}
 
