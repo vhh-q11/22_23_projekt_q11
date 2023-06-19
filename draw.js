7d40650e71d4c2f43eed6764aeaf7f1408915238
vhh-q11 <133121252+vhh-q11@users.noreply.github.com>
2023-06-19 10:29:52 +0200

    Update draw.js

--- draw.js
+++ draw.js
@@ -3,12 +3,43 @@ class Drawpad {
     constructor(d) {
         this.drawingArea = d;
         var ctx = d.getContext("2d");
+        this.#addEventListeners();
         ctx.moveTo(0, 0);
         ctx.lineTo(100, 100);
         ctx.stroke();
 
 
     }
+
+
+    #addEventListeners() {  // eine private Methode
+        this.drawingArea.onmousemove = (evt) => {
+            if (this.mousePressed == true) {
+                const rect = this.drawingArea.getBoundingClientRect(); // hole die Koordinaten der Drawingarea
+                const x = evt.clientX - rect.left;
+                const y = evt.clientY - rect.top;
+                console.log("x: " + x + " y: " + y);
+                this.drawingArea.getContext("2d").lineTo(x, y);
+                this.drawingArea.getContext("2d").stroke();
+            }
+        }
+
+        this.drawingArea.onmousedown = (evt) => {
+            console.log("mouse pressed");
+            this.mousePressed = true;
+        }
+
+        this.drawingArea.onmouseup = (evt) => {
+            console.log("mouse released");
+            this.mousePressed = false;
+        }
+
+
+
+
+    }
+
+
 
 
 
